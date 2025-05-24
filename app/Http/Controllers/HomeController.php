0692c0c841b2905645e9fd38370892fcf7c03ea7
minhhiep02112001 <chuminhhiep0211@gmail.com>
2025-05-24 23:09:39 +0700

    u

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -74,7 +74,7 @@ class HomeController extends Controller
             'meta_keyword' => $post->title ?? '',
             'is_robot' => $post->is_robot ?? 0,
             'image' => $post->thumbnail ?? config('data.cms_setting.logo'),
-            'favicon' => getImageThumb($post->thumbnail, 100, 100),
+
             'url' => route('post', ['slug' => $post->slug]),
         ];
         $breadcrumbs = [array('url' => '', 'title' => $post->title)];
@@ -89,6 +89,7 @@ class HomeController extends Controller
         ];
 
         if ($post->type == 'brand') {
+            $SEO['favicon'] =   getImageThumb($post->thumbnail, 100, 100);
             $data['relates'] = Post::where([
                 'type' => 'brand',
                 'is_status' => 1,
