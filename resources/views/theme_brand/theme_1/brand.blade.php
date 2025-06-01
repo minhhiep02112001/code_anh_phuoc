705d644c2b493ce2f403391acffec89fc8f28d82
minhhiep02112001 <chuminhhiep0211@gmail.com>
2025-06-02 02:12:43 +0700

    update

--- resources/views/theme_brand/theme_1/brand.blade.php
+++ resources/views/theme_brand/theme_1/brand.blade.php
@@ -417,7 +417,7 @@
                                              title="{{ $item->title }}">
                                              <div class="image-box">
                                                  <figure class="image" style="aspect-ratio: 3/2;">
-                                                     {!! getThumbnailImg($item, 600, 400) !!}
+                                                     {!! getThumbnailImg($item->thumbnail, 600, 400) !!}
                                                  </figure>
                                              </div>
                                              <div class="lower-content">
