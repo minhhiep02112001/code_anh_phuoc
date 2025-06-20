a40ac7c88db7c006c6072acf0c6b87b2b3d64112
minhhiep02112001 <chuminhhiep0211@gmail.com>
2025-06-21 00:42:44 +0700

    update

--- resources/views/front_end/_index.blade.php
+++ resources/views/front_end/_index.blade.php
@@ -1,5 +1,5 @@
 @php
-    $ver = 116;
+    $ver = 136;
     $config_website = getValueSetting('config_website');
     $config_seo = getValueSetting('config_seo');
     $config_social = getValueSetting('config_social');
