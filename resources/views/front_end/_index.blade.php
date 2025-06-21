05e11d30a24ca8526f1337246c0db1f9c74d8bd7
minhhiep02112001 <chuminhhiep0211@gmail.com>
2025-06-21 23:17:26 +0700

    Remove large

--- resources/views/front_end/_index.blade.php
+++ resources/views/front_end/_index.blade.php
@@ -1,5 +1,5 @@
 @php
-    $ver = 1368;
+    $ver = 11368;
     $config_website = getValueSetting('config_website');
     $config_seo = getValueSetting('config_seo');
     $config_social = getValueSetting('config_social');
