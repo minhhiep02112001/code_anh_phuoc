c7b25896a277d5cf8369c72c1cb84d8a8076e08a
minhhiep02112001 <chuminhhiep0211@gmail.com>
2025-06-22 00:09:48 +0700

    R

--- app/Console/Commands/ConvertData.php
+++ app/Console/Commands/ConvertData.php
@@ -112,7 +112,7 @@ class ConvertData extends Command
                 DB::table('st_post')->where('id', $post->id)->update($data_update);
                 DB::table('crawler_map')->where('id', $data->id)->update(['relate_id' => $post->id]);
                 DB::table('st_post_images')->where('crawler_id', $data->id)->update(['post_id' => $post->id]);
-                echo "\n Done $post->id";
+                echo "\n Done {$post->id} status {$post->is_status}";
             }
         }
         die("Done All");
