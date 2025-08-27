483eee478812d4d861e6268af3068fca07d5cc99
@hiepcm <hiepcm@imap.edu.vn>
2025-08-27 08:38:33 +0700

    update

--- app/Console/Commands/ConvertData.php
+++ app/Console/Commands/ConvertData.php
@@ -42,91 +42,132 @@ class ConvertData extends Command
 
 
 
-    // php artisan convert:data --function=updatePost
     public function updatePost()
     {
-
-        $datas = DB::table('crawler_map')->where([
+        $datas = Crawler::where([
+            'domain' => env('APP_URL'),
             'is_crawler' => 1,
-            'is_status' => 2
+            'is_status' => 1,
+            'is_convert' => 0
         ])->get();
 
         foreach ($datas as $data) {
-            if (empty($data->slug))  $data->slug = \Str::slug($data->key_word);
-            $post = Post::firstOrCreate(['slug' => $data->slug], ['title' => $data->key_word, 'slug' => $data->slug]);
-           
-            if (!empty($post->is_status)) continue;
-            
-            $data_update = [
-                'is_thumb_block_1' => 1,
-                'is_thumbnail' => 1
-            ];
-            $thumnail_post = str_replace(['storage', '//'], '', trim($post->thumbnail ?? '', '/'));
+            try { 
+                $data->slug = \Str::slug($data->key_word);
+                $post = Post::firstOrCreate(['slug' => $data->slug], ['title' => $data->key_word, 'slug' => $data->slug]);
+                if (!empty($post->is_status)) continue;
+                echo "\n Start " . $data->key_word;
 
-            if ((empty($thumnail_post) || !Storage::disk('public')->exists($thumnail_post)) && !empty($data->thumbnail)) { // download_image
-                $data_update['thumbnail'] = saveImageUrlStorage($data->thumbnail, "photos/nails/{$post->slug}",   "thumbnail.jpg");
-                $thumnail_post = str_replace(['storage', '//'], '', trim($data_update['thumbnail'], '/'));
-            }
+                $data_update = [
+                    'is_thumbnail' => 1,
+                    'slug' => $data->slug,
+                ];
 
-            if (Storage::disk('public')->exists($thumnail_post))  $data_update['is_thumbnail'] = 0;
+                $thumnail_post = str_replace(['storage', '//'], '', trim($post->thumbnail ?? '', '/'));
 
-            $thumbs = Media::where('post_id', $post->id)->orderBy('id', 'asc')->get();
-            if ($thumbs->isNotEmpty()) {
-                if ($thumbs->where('type', 'banner')->count() == 0) {
-                    $arrs = $thumbs->whereIn('type', ['photo', 'menu'])->take(3)->pluck('id')->toArray();
-                    Media::whereIn('id',  $arrs)->update(['type' => 'banner']);
+                if ((empty($thumnail_post) || !Storage::disk('public')->exists($thumnail_post)) && !empty($data->thumbnail)) { // download_image
+                    $data_update['thumbnail'] = saveImageUrlStorage($data->thumbnail, "photos/nails/{$post->slug}",   "thumbnail.jpg");
+                    $thumnail_post = str_replace(['storage', '//'], '', trim($data_update['thumbnail'], '/'));
                 }
 
-                $imgBlock = Media::where(['post_id' => $post->id, 'type' =>  'block'])->first();
-                if (empty($imgBlock)) {
-                    $imgBlock = Media::where('post_id', $post->id)->whereIn('type', ['photo', 'menu'])->first();
+                if (Storage::disk('public')->exists($thumnail_post))  $data_update['is_thumbnail'] = 0;
+
+                $thumbs = Media::where('post_id', $post->id)->orderBy('id', 'asc')->get();
+
+                if ($thumbs->isEmpty()) {
+                    $images = DB::table('datacenter.st_images')->where('crawler_id', $data->id)->get();
+
+                    $data_insert = collect($images)->take(20)->map(function ($item) use ($post) {
+                        return [
+                            'thumbnail' => $item->thumbnail,
+                            'crawler_href' => $item->crawler_href,
+                            'post_id'   => $post->id,
+                            'position'  => $item->position ?? 0,
+                            'type'      => 'photo',
+                        ];
+                    })->toArray();
+                    if (!empty($data_insert)) {
+                        Media::insert($data_insert);
+                        $thumbs = Media::where('post_id', $post->id)->orderBy('id', 'asc')->get();
+                        echo "\n Insert " . $thumbs->count() . " thumb";
+                    }
                 }
-                if (!empty($imgBlock)) {
-                    Media::where('id',  $imgBlock->id)->update(['type' => 'block']);
-                    $data_update['image_block_1'] = $imgBlock->thumbnail ?? '';
-                    $data_update['is_thumb_block_1'] = 0;
+
+                $abouts = About::where('relate_id', $post->id)->orderBy('id', 'asc')->get();
+
+                if ($abouts->isEmpty()) {
+                    $dataAbout = DB::table('datacenter.about')->where('crawler_id', $data->id)->get();
+
+                    if ($dataAbout->isNotEmpty()) {
+                        foreach (collect($dataAbout)->where('parent_id', 0) as $val) {
+                            $children = collect($dataAbout)->where('parent_id', $val->id)->values()->toArray();
+
+                            $aboutId = About::insertGetId([
+                                'title' => $val->title,
+                                'slug' => $val->slug,
+                                'crawler_id' => $val->crawler_id,
+                                'parent_id' => 0,
+                                'relate_id' => $post->id
+                            ]);
+                            if (!empty($children)) {
+                                $dataInsertAbout = array_map(function ($i)  use ($aboutId, $post) {
+                                    return [
+                                        'title' => $i->title,
+                                        'slug' => $i->slug,
+                                        'crawler_id' => $i->crawler_id,
+                                        'parent_id' => $aboutId,
+                                        'relate_id' => $post->id
+                                    ];
+                                }, $children);
+                                About::insert($dataInsertAbout);
+                            }
+                        }
+                        echo "\n Insert " . $dataAbout->count() . " about";
+                    }
                 }
-            }
 
+                if (!empty($data->time_open)) {
+                    $data_update['time_open']  = convertTimeOpen($data->time_open);
+                }
 
-            if (!empty($data->time_open)) {
-                $data_update['time_open']  = convertTimeOpen($data->time_open);
-            }
+                if (!empty($data->address)) {
+                    $_address = str_replace(['Address:', '\u{A0}', ':'], '', $data->address);
+                    $data_update['address'] = trim(str_replace('  ', ' ', $_address));
+                }
 
-            if (!empty($data->address)) {
-                $_address = str_replace(['Address:', '\u{A0}', ':'], '', $data->address);
-                $data_update['address'] = trim(str_replace('  ', ' ', $_address));
-            }
+                if (!empty($data->google_review)) {
+                    $data_update['review_google'] = trim(str_replace(['avis Google', ' ', '\u{A0}',], '', $data->google_review));
+                    $data_update['review_google'] = (int) $data_update['review_google'];
+                }
 
-            if (!empty($data->google_review)) {
-                $data_update['review_google'] = trim(str_replace(['avis Google', ' ', '\u{A0}',], '', $data->google_review));
-                $data_update['review_google'] = (int) $data_update['review_google'];
-            }
+                if (!empty($data->phone)) {
+                    $data_update['phone'] = str_replace(['Phone: ', ':', '\u{A0}',], '', $data->phone);
+                    $data_update['phone'] = trim(str_replace('  ', ' ', $data_update['phone']));
+                }
 
-            if (!empty($data->phone)) {
-                $data_update['phone'] = str_replace(['Phone: ', ':', '\u{A0}',], '', $data->phone);
-                $data_update['phone'] = trim(str_replace('  ', ' ', $data_update['phone']));
-            }
+                if (!empty($data->link_google_map)) {
+                    $data_update['link_map'] =  $data->link_google_map;
+                }
 
-            if (!empty($data->link_google_map)) {
-                $data_update['link_map'] =  $data->link_google_map;
-            }
+                if (!empty($data->iframe_map)) {
+                    $data_update['iframe_map'] =  $data->iframe_map;
+                }
 
-            if (!empty($data->iframe_map)) {
-                $data_update['iframe_map'] =  $data->iframe_map;
-            }
-
-            if (!empty($data_update)) {
-                //     // $data_update['is_status'] = 0;
-                DB::table('st_post')->where('id', $post->id)->update($data_update);
-                DB::table('crawler_map')->where('id', $data->id)->update(['relate_id' => $post->id]);
-                DB::table('st_post_images')->where('crawler_id', $data->id)->update(['post_id' => $post->id]);
-                // echo "\n Done {$post->id} status {$post->is_status}";
-                echo "\n Done {$post->id} status {$post->is_status} | status thumb {$data_update['is_thumbnail']} | status block {$data_update['is_thumb_block_1']}";
+                if (!empty($data_update)) {
+                    //     // $data_update['is_status'] = 0;
+                    DB::table('st_post')->where('id', $post->id)->update($data_update);
+                    Crawler::where('id', $data->id)->update(['relate_id' => $post->id, 'is_convert' => 1]);
+                    // echo "\n Done {$post->id} status {$post->is_status}";
+                }
+                echo "\n Done {$post->id} ============";
+            } catch (\Exception $ex) {
+                echo "\n Error {$data['title']}";
             }
         }
         die("Done All");
     }
+
+    
     public function getRandomTheme()
     {
         $arr = config('theme_colors.colors');
