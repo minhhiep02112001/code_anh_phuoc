953ab8d891c992b1ebc3a767429bd75017d8fd6d
@hiepcm <hiepcm@imap.edu.vn>
2025-12-18 16:55:14 +0700

    update

--- resources/views/front_end/_index.blade.php
+++ resources/views/front_end/_index.blade.php
@@ -1,5 +1,5 @@
 @php
-    $ver = 11368;
+    $ver = 368;
     $config_website = getValueSetting('config_website');
     $config_seo = getValueSetting('config_seo');
     $config_social = getValueSetting('config_social');
