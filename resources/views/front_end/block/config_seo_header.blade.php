ded9ddd4681b93daec75609995ae008752756306
@hiepcm <hiepcm@imap.edu.vn>
2026-01-06 15:06:55 +0700

    update

--- resources/views/front_end/block/config_seo_header.blade.php
+++ resources/views/front_end/block/config_seo_header.blade.php
@@ -57,10 +57,10 @@
     {!! $config_website->schema ?? '' !!}
 @endif
 
-<meta name="robots" content="noindex,nofollow" />
-<meta name="Googlebot-News" content="noindex,nofollow">
+{{-- <meta name="robots" content="noindex,nofollow" />
+<meta name="Googlebot-News" content="noindex,nofollow"> --}}
 
-{{-- <meta name="robots" content="{{ !empty($config_seo->index) ? 'index,follow' : 'noindex,nofollow' }}" />
-<meta name="Googlebot-News" content="{{ !empty($config_seo->index) ? 'index,follow' : 'noindex,nofollow' }}"> --}}
+<meta name="robots" content="{{ !empty($config_seo->index) ? 'index,follow' : 'noindex,nofollow' }}" />
+<meta name="Googlebot-News" content="{{ !empty($config_seo->index) ? 'index,follow' : 'noindex,nofollow' }}">
 <link rel="canonical" href="{{ url()->current() }}" />
 {!! $config_website->config_header ?? '' !!}
