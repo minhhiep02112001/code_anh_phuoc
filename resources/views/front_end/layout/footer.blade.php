cca19367ad874c60aad274979c376750d197b011
minhhiep02112001 <chuminhhiep0211@gmail.com>
2026-01-24 10:16:13 +0700

    update composer

--- resources/views/front_end/layout/footer.blade.php
+++ resources/views/front_end/layout/footer.blade.php
@@ -12,7 +12,7 @@
                                 alt="{{ $config_website->website ?? '' }}">
                         </a>
                         @if (!empty($config_website->content_footer))
-                            <div>
+                            <div class="text-white">
                                 {!! $config_website->content_footer !!}
                             </div>
                         @endif
@@ -23,7 +23,8 @@
                         <h5 class="title-footer">About Us</h5>
                         <ul class="footer-nav">
                             @foreach ($menus_footer as $menu)
-                                <li><a href="{{ $menu->link }}" title="{{ $menu->title }}">{{ $menu->title }}</a>
+                                <li><a href="{{ $menu->link }}" class="text-white"
+                                        title="{{ $menu->title }}">{{ $menu->title }}</a>
                                 </li>
                             @endforeach
                         </ul>
@@ -32,7 +33,7 @@
                 <div class="col-lg-4 col-sm-12">
                     <h5 class="title-footer">Do Business With Us</h5>
                     <ul class="footer-nav">
-                        <li>Email : {{ $config_website->email ?? '' }}</li>
+                        <li class="text-white">Email : {{ $config_website->email ?? '' }}</li>
                     </ul>
                     <ul class="social-icon-one">
                         <li><a href="#"><span class="fab fa-facebook"></span></a></li>
@@ -47,6 +48,6 @@
         </div>
     </div>
     <div class="footer-bottom">
-        <div class="text">© 2025 {{ $config_website->website ?? '' }}. All rights reserved.</div>
+        <div class="text  text-white">© 2025 {{ $config_website->website ?? '' }}. All rights reserved.</div>
     </div>
 </footer>
