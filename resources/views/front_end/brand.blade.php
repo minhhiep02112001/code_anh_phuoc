8bf598bb89a680e5ff2a933344dbf264b06841a3
minhhiep02112001 <chuminhhiep0211@gmail.com>
2026-04-27 09:26:28 +0700

    update

--- resources/views/front_end/brand.blade.php
+++ resources/views/front_end/brand.blade.php
@@ -53,15 +53,15 @@
         #reviews .hide {
             display: none;
         }
-   button.loadmoreReview
-     {
-    padding: 10px;
-    border-radius: 10px;
-    margin-bottom: 20px;
-    background: bisque;
-    margin: 0 auto;
-    display: block;
-} 
+
+        button.loadmoreReview {
+            padding: 10px;
+            border-radius: 10px;
+            margin-bottom: 20px;
+            background: bisque;
+            margin: 0 auto;
+            display: block;
+        }
     </style>
     <section class="listing-banner box-brand">
         <div class="background-layer banner-brand" style="background-image: url('{{ getImageThumb($post->thumbnail) }}');">
@@ -71,9 +71,9 @@
                 <div class="menu-item header-fixed">
                     <ul>
                         <li class="show"><a class="active" href="#overview" title="Overview">Overview</a></li>
-                        <li class="show"><a href="#photos" title="Photos">Photos</a></li>
                         <li class="show"><a href="#menu" title="Menu">Menu</a></li>
                         <li class="show"><a href="#reviews" title="Reviews">Reviews</a></li>
+                        <li class="show"><a href="#photos" title="Photos">Photos</a></li>
                         <li class="show"><a href="#location" title="Location">Location</a></li>
                     </ul>
                 </div>
@@ -196,6 +196,42 @@
                             </div>
                         @endif
 
+                        @if (!empty($comments))
+                            <div class="comments-widget ls-widget" id="reviews">
+                                <div class="widget-title">
+                                    <h2><span class="icon flaticon-consulting-message"></span> Reviews {{ $post->title }}
+                                    </h2>
+                                </div>
+                                <div class="widget-content listReview">
+
+                                    @foreach (collect($comments)->values() as $k => $item)
+                                        <div class="comment {{ $k < 5 ? 'show' : 'hide' }}"
+                                            data-index="{{ $k }}">
+                                            <div class="user-name"> {{ $item->fullname }}</div>
+                                            <div class="comment-info listing-block-two">
+                                                <ul class="rating">
+                                                    <span class="fa fa-star"></span>
+                                                    <span class="fa fa-star"></span>
+                                                    <span class="fa fa-star"></span>
+                                                    <span class="fa fa-star"></span>
+                                                    <span class="fa fa-star"></span>
+                                                </ul>
+                                                <div class="comment-time">
+                                                    {{ format_date($item->created_at, 'd-m-Y') }}
+                                                </div>
+                                            </div>
+                                            <div class="text">
+                                                {!! $item->content ?? '' !!}
+                                            </div>
+                                        </div>
+                                    @endforeach
+
+                                </div>
+                                @if (collect($comments)->count() > 0)
+                                    <button class="loadmoreReview">See more reviews</button>
+                                @endif
+                            </div>
+                        @endif
 
                         @if (!empty($photos))
                             <div class="gallery-widget ls-widget" id="photos">
@@ -225,41 +261,7 @@
                             </div>
                         @endif
 
-                        @if (!empty($comments))
-                            <div class="comments-widget ls-widget" id="reviews">
-                                <div class="widget-title">
-                                    <h2><span class="icon flaticon-consulting-message"></span> Reviews {{ $post->title }}
-                                    </h2>
-                                </div>
-                                <div class="widget-content listReview">
 
-                                    @foreach (collect($comments)->values()  as $k => $item) 
-                                        <div class="comment {{ $k < 5 ? 'show' : 'hide' }}" data-index="{{  $k }}">
-                                            <div class="user-name"> {{ $item->fullname }}</div>
-                                            <div class="comment-info listing-block-two">
-                                                <ul class="rating">
-                                                    <span class="fa fa-star"></span>
-                                                    <span class="fa fa-star"></span>
-                                                    <span class="fa fa-star"></span>
-                                                    <span class="fa fa-star"></span>
-                                                    <span class="fa fa-star"></span>
-                                                </ul>
-                                                <div class="comment-time">
-                                                    {{ format_date($item->created_at, 'd-m-Y') }}
-                                                </div>
-                                            </div>
-                                            <div class="text">
-                                                {!! $item->content ?? '' !!}
-                                            </div>
-                                        </div>
-                                    @endforeach
-
-                                </div>
-                                @if (collect($comments)->count() > 0)
-                                    <button class="loadmoreReview">See more reviews</button>
-                                @endif
-                            </div>
-                        @endif
                         <div class="comments-form-widget ls-widget">
                             <div class="widget-title">
                                 <h4><span class="icon flaticon-consulting-message"></span> Add a Review</h4>
